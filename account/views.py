3e3d2755f6c47d7367ada680ca44850eca893da3
yun <sys394880@gmail.com>
2022-01-09 00:37:01 +0900

    commit 5 : 회원가입 fetch API를 이용한 데이터 전송 구현 중인데 django Forbidden (CSRF token missing.) 문제 생김. 해결법은 찾았으나 정확히 잘 모르는 관계로 분석한 후 재 커밋할 것.

--- account/views.py
+++ account/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 def login(request):
@@ -6,4 +8,9 @@ def login(request):
 
 
 def sign_up(request):
+    print("request test!")
+    if request.method == "POST":
+        print(request)
+        print(request.POST)
+        return JsonResponse({"confirm" : True})
     return render(request, 'sign_up.html')
