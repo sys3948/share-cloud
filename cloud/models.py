2694f9e3d1a30e33e2c06dc6d2ffc6e384358ba9
yun <sys394880@gmail.com>
2022-02-22 01:56:15 +0900

    commit 27 : cloud Model 변경

--- cloud/models.py
+++ cloud/models.py
@@ -1,3 +1,4 @@
+from pyexpat import model
 from django.db import models
 from django.db.models.deletion import CASCADE, SET_DEFAULT
 
@@ -6,8 +7,7 @@ class FileFolder(models.Model):
     folder_id = models.IntegerField(default=1)
     folder_name = models.CharField(max_length=300)
     oner_id = models.ForeignKey('account.Account', on_delete=CASCADE, related_name='oner_id',)
-    share_id = models.IntegerField(default=0)
-    upper_folder_id = models.IntegerField()
+    upper_folder_id = models.ForeignKey('self', on_delete=CASCADE, related_name='upper_id')
     level = models.IntegerField(default=0)
 
 
@@ -17,6 +17,11 @@ class StoreFile(models.Model):
     create_date = models.DateTimeField(auto_now=True)
     upload_date = models.DateTimeField(auto_now_add=True)
     user_id = models.ForeignKey('account.Account', on_delete=CASCADE, related_name='account')
-    folder_id = models.IntegerField(default=1)
+    belong_folder_id = models.ForeignKey(FileFolder, on_delete=CASCADE, related_name='belong_folder_id')
 
+
+class ShareFolder(models.Model):
+    share_folder_id = models.ForeignKey(FileFolder, on_delete=CASCADE, related_name='share_folder_id')
+    onner_id = models.ForeignKey('account.Account', on_delete=CASCADE, related_name='onner_id')
+    share_user_id = models.ForeignKey('account.Account', on_delete=CASCADE, related_name='share_id')
 
