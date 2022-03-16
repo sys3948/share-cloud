0cf447806bf3b7e178accd5c2418d8114877f939
yun <sys394880@gmail.com>
2022-03-16 23:54:51 +0900

    commit 38 : Folder 생성하기 위한 path가 엉뚱하게 되어있는 부분 수정 및 다른 문제 발생(문제 내용은 ReadMe 3 - 16일자 참고)

--- cloud/views.py
+++ cloud/views.py
@@ -55,7 +55,10 @@ def main(request):
 
         upper_folder_path = ''
         if folder.upper_folder_id:
-            upper_folder_path = cryptocode.encrypt(str(folder.upper_folder_id.folder_id), settings.KEY)
+            f_id = [f for f in folder.folder_path.split('/')][1:-1]
+            for f_p in f_id:
+                upper_folder_path += str(FileFolder.objects.get(id = int(f_p)).folder_id) + '/'
+            upper_folder_path = cryptocode.encrypt(upper_folder_path, settings.KEY)
 
         folder_path = cryptocode.encrypt(str(folder.folder_id), settings.KEY)
 
@@ -81,16 +84,19 @@ def main(request):
     if FileFolder.objects.filter(oner_id = request.session.get('id')).exists():
         folders_data = FileFolder.objects.filter(oner_id = request.session.get('id'))
     if ShareFolder.objects.extra(where=["onner_id_id = %s OR share_user_id_id = %s"], params=[request.session.get('id'), request.session.get('id')]).exists():
-        share_folders_data = ShareFolder.objects.extra(where=["onner_id_id = %s OR share_user_id_id = %s"], params=[request.session.get('id'), request.session.get('id')]).order_by('timestamp')
+        share_folders_data = ShareFolder.objects.extra(where=["onner_id_id = %s OR share_user_id_id = %s"], params=[request.session.get('id'), request.session.get('id')]).values('groupnum').values()
 
     if share_folders_data:
+        print(share_folders_data)
+        print('-' * 100)
         for share_folder in share_folders_data:
-            print(share_folder.share_folder_id)
-            print(share_folder.share_folder_id.id)
-            print(share_folder.share_folder_id.folder_name)
-            print(share_folder.share_folder_id.upper_folder_id)
-            if share_folder.share_folder_id.upper_folder_id:
-                print(share_folder.share_folder_id.upper_folder_id.folder_name)
+            print(share_folder.values())
+            # print('Column Values' + '===' * 10)
+            # print(share_folder.get('share_folder_id_id'))
+            # print(share_folder.share_folder_id.folder_name)
+            # print(share_folder.share_folder_id.upper_folder_id)
+            # if share_folder.share_folder_id.upper_folder_id:
+            #     print(share_folder.share_folder_id.upper_folder_id.folder_name)
 
     return render(request, 'main.html', {'folders_data' : folders_data, 'share_folders_data' : share_folders_data})
 
